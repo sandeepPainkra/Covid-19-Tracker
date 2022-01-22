9f172feb142595bfa2e5844a30e51154cbd4b197
Sandeep <samdeeppainkra@gmail.com>
2022-01-23 00:56:52 +0530

    creating header section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import { FormControl, MenuItem, Select } from "@material-ui/core";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [country, setCountry] = useState(["USA", "UK", "India"]);
+  console.log(country);
   return (
     <div className="App">
       <h2>
-        hey buddy whatsapp lets build a covid-19 tracker app using React js
+        hey buddy whatsapp lets build a covid-19 tracker app using React js!!
       </h2>
+      <div className="app__header">
+        <h1>Covid 19 Tracker</h1>
+        <FormControl>
+          <Select variant="outlined" value="abc">
+            {country.map((country) => {
+              return <MenuItem vlaue={country}>{country}</MenuItem>;
+            })}
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
